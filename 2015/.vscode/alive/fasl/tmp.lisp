9da903579d6424741d5b646232a114f87a78f732
Giles Malet <gdmalet@gmail.com>
2022-12-07 13:21:43 -0500

    Organise files by year.

---
+++ 2015/.vscode/alive/fasl/tmp.lisp
@@ -0,0 +1,23 @@
+;;;; Advent of code, day 1 problem 1.
+;;;; Answer is 138.
+
+(defconstant *input-file* "input.txt"
+  "Where we read the directions.")
+
+(defun main()
+  (format t "Santa ends on floor ~A.~%" (count-floors *input-file*)))
+
+(defun count-floors(from-file)
+  (with-open-file
+      (f (make-pathname :name from-file)
+         :direction :input
+         :if-does-not-exist :error)
+
+    (loop
+       with floor = 0
+       for brace = (read-char f nil)
+       do
+         (ecase brace
+           (#\( (incf floor))
+           (#\) (decf floor))
+           ((nil) (return-from count-floors floor))))))
