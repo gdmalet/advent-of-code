8df5af3c5bfae718872dac3883e2a0f39c53272e
Giles Malet <gdmalet@gmail.com>
2022-12-09 16:39:31 -0500

    Day 04 problem 1 complete.

---
+++ 2022/04/day04p1.lisp
@@ -0,0 +1,57 @@
+;;;; Advent of code, day 4 problem 2.
+;;;; Answer is 657.
+
+(defconstant +input-file+ "input.txt"
+  "Where we read the goods.")
+
+(defun split-range (range)
+  "Splits string of two dash-separated numbers, and return the numbers."
+  
+  ;; Make our own copy of the readtable, so we can change it
+  (let ((*readtable* (copy-readtable)))
+	;; turn the dash into a separator
+	(set-macro-character
+	 #\- (lambda (stream char)
+		   (declare (ignore char))
+		   (read stream t nil t)))
+
+	(multiple-value-bind (left end)
+		(read-from-string range)
+	  (values left (read-from-string range nil nil :start end)))))
+
+(defun process-line (left right)
+  "Returns 1 if one range fully contains the other, else 0."
+  (multiple-value-bind (left-from left-to)
+	  (split-range left)
+	(multiple-value-bind (right-from right-to)
+		(split-range right)
+	  (if (or (and (>= left-from right-from)
+				   (<= left-to right-to))
+			  (and (>= right-from left-from)
+				   (<= right-to left-to)))
+		  1
+		  0))))
+
+(defun main ()
+   (with-open-file
+       (f (make-pathname :name +input-file+)
+         :direction :input
+         :if-does-not-exist :error)
+
+	 ;; Make our own copy of the readtable, so we can change it
+	 (let ((*readtable* (copy-readtable)))
+
+	   ;; The lisp reader would normally barf on a raw comma, so instead
+	   ;; make it a delimiter which is otherwise ignored.
+	   (set-macro-character
+		#\, (lambda (stream char)
+			  (declare (ignore char))
+			  (read stream t nil t)))
+ 
+     (loop
+       for left = (read f nil)
+       for right = (read f nil)
+       while left
+       summing (process-line (symbol-name left)
+							 (symbol-name right)) into total
+       finally (return total)))))
